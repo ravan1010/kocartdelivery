377167ecb7cd60072c56007114102cf74dbfe80a
ravan1010 <ravanravana177@gmail.com>
2026-06-22 15:45:24 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
       // baseURL: 'https://api.kocart.online/',
-  baseURL: 'http://localhost:5001',
+  // baseURL: 'http://localhost:5001',
+    baseURL: 'https://serverside.kocart.online',
+
   withCredentials: true,
 });
 
