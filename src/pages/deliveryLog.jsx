cf995001a7c07bc66f8129f24bc3f8c488675c50
ravan1010 <ravanravana177@gmail.com>
2026-06-22 14:44:46 +0530

    first commit

--- src/pages/deliveryLog.jsx
+++ src/pages/deliveryLog.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function DeliveryLog() {
   const handleGoogleLogin = () => {
-    window.open("http://localhost:5001/auth/google/deliveryBoy", "_self");
+    window.open("https://serverside.kocart.online/auth/google/deliveryBoy", "_self");
   };
 
   return (
